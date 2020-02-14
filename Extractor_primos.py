03ce14a950739a9f9d4af899cc3dbb0d1f510778
Alekla0126 <alekla0126@gmail.com>
2020-02-13 19:08:02 -0600

    Version final, listas dinamicas, orientado a objetos y entradas del usuario

--- Extractor_primos.py
+++ Extractor_primos.py
@@ -2,28 +2,32 @@ import math
 
 class Extractor(object):
 
-    def __init__(self):
-        self.list = [0 for i in range(1001)]
+    def __init__(self,low,high):
+        self.list = []
         self.primo = []
+        self.low = low
+        self.high = high
 
     def initizalize(self):
-        index = 0
-        while(index<1001):
-            self.list[index] = index
+        index = self.low
+        while(index<self.high+1):
+            self.list.insert(index,index)
             index = index + 1
         return list
     
     def extract(self):
         self.primo.clear
         prime = False
-        for i in range(0,1001):
+        for i in range(self.low,self.high):
             if(i>0):
                 if ((math.factorial(i-1))+1) % i == 0:
                     self.primo.append(self.list[i])         
         return self.primo
 
 def main():
-    extractor = Extractor()
+    low = int (input("Limite inferiror"))
+    high = int (input("Limite superior"))
+    extractor = Extractor(low,high)
     extractor.initizalize()
     primos = extractor.extract()
     print(primos)
